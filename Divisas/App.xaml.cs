11c2627b67a60bbf498b5e755c206e67b988793f
JosueEPN <josue-S15@hotmail.com>
2022-08-04 23:25:52 -0500

    HomePage

--- Divisas/App.xaml.cs
+++ Divisas/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Divisas.Pages;
+using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -10,7 +11,8 @@ namespace Divisas
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            //MainPage = new MainPage();
+            MainPage = new NavigationPage(new Home());
         }
 
         protected override void OnStart()
